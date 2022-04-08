75fa0d1321375420545bca1c6f6f5120a74e3c05
Safwan Rashid <safwanrashid.t@gmail.com>
2022-04-08 12:04:31 +0530

    Register page Created
    Routing for register page is done both login and register navigation is
    working fine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
-import LoginComponent from "./components/LoginComponent";
 import { Route, Routes } from 'react-router-dom';
+// components
+import LoginComponent from "./components/LoginComponent";
+import RegisterComponent from "./components/RegisterComponent";
 
 function App() {
   return (
     <Routes>
       <Route path={'/'} element={<LoginComponent />} />
+      <Route path={'/register'} element={<RegisterComponent />} />
     </Routes>
   );
 }
---
+++ src/components/RegisterComponent.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+function RegisterComponent() {
+    return ( 
+        <form className="w-25 mx-auto mt-5 border p-5">
+      <h1 className="text-center mb-3">Register</h1>
+      <div className="mb-3">
+        <label className="form-label">
+            full Name 
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        ></input>
+      </div>
+      <div className="mb-3">
+        <label className="form-label">
+            Mobile number
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="mobileNumber"
+          name='Mobile number'
+        ></input>
+      </div>
+      <div className="text-center">
+      <button type="submit" className="btn btn-lg btn-primary">
+        Register
+      </button>
+      </div>
+      <div className="mt-3 text-center">
+      <p className="d-inline form-text">Existing user </p><Link className="form-text" to={'/'}>Login</Link>
+      </div>
+    </form>
+     );
+}
+
+export default RegisterComponent;
